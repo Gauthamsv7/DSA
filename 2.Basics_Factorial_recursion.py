ced2b13634f8384a5d44e819c1fdbb80bb8b255b
gautham_sv <sv.gautham7@gmail.com>
2024-05-25 17:11:51 +0530

    Basic Factorial with Recursion

---
+++ 2.Basics_Factorial_recursion.py
@@ -0,0 +1,20 @@
+#Recursive Func : A Function calling itself
+
+def recursive_fatorial(n):
+    if n == 0:
+        return 1
+    
+    return n * recursive_fatorial(n-1)
+
+def main():
+    try:
+        n = int(input("Please Enter the Number : "))
+        print("Finale Value : ",recursive_fatorial(n))
+        print("All Values : \n")
+        for i in range(1,n+1):
+            print(i,recursive_fatorial(i))
+    except ValueError:
+        print("Please Enter a Valid Number")
+        
+if __name__ == "__main__":
+    main()
